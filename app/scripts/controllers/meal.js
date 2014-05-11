ea2610aa58080982eb56d91fd20f21904d30d149
Nicolas Arkkila <nicolas@arkkila.fi>
2014-05-11 23:55:42 +0300

    Introducing: meals

--- app/scripts/controllers/meal.js
+++ app/scripts/controllers/meal.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('rkcApp')
-  .controller('MealCtrl', function ($scope, $http) {
+  .controller('MealCtrl', function ($scope, $http, Cartlink) {
     $http.get('/api/meal').success(function(meal) {
+        
       $scope.meal = meal;
+      $scope.starter = meal.starter;
+      $scope.maincourse = meal.maincourse;
+      $scope.desserts = meal.desserts;
+      $scope.link = Cartlink.generateLink(meal.cart);
+
     });
   });
